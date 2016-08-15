6fb7e1b154dd992a2e312cb3fe41657108915b81
Roman Teleshun <teleshun555@gmail.com>
2016-08-16 00:48:51 +0200

    add maine logic

--- src/test/java/com/tests/Tests.java
+++ src/test/java/com/tests/Tests.java
@@ -1,24 +1,32 @@
 package com.tests;
 
-import com.framework.flow.ActionsFlow;
+import com.framework.checker.Calculator;
+import com.framework.checker.Operations;
+import com.framework.page.CalculatorPage;
+import com.framework.page.elements.table.HistoryTable;
 import com.framework.util.PropertyLoader;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.awt.*;
 import java.io.File;
 import java.util.concurrent.TimeUnit;
 
+import static com.framework.checker.Operations.*;
+import static com.framework.util.Helper.COLUMN_TO_VALIDATE;
+import static com.framework.util.Helper.ROW_TO_VALIDATE;
 import static com.framework.util.PropertyLoader.PROP_FILE;
-import static com.framework.util.PropertyLoader.TEST_DATA;
 
 public class Tests {
 
-    WebDriver driver;
+    private WebDriver driver;
+	private CalculatorPage calculatorPage;
+	private HistoryTable table;
+
 
 	@BeforeTest
 	public void setup(){
@@ -27,17 +35,77 @@ public class Tests {
 		driver = new ChromeDriver();
 
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.navigate().to( PropertyLoader.loadProperty("url", PROP_FILE) );
 
-		driver.navigate().to(PropertyLoader.loadProperty("url", PROP_FILE));
-		driver.manage().window().maximize();
+		calculatorPage = new CalculatorPage(driver);
+		table = new HistoryTable(driver);
+
 	}
 
 
-	@Test
-	public void sendFileTest() throws InterruptedException, AWTException {
 
-		Assert.assertTrue(true);
+	@Test(dataProvider = "ValidDataProvider")
+	public void verifyPositive(String firstNum, String secondNum, Operations value) {
+		String res = Calculator.calculate(firstNum,secondNum,value);
+		calculatorPage.setDataAndRun(firstNum,secondNum,value,res);
+		table.load();
+		Assert.assertTrue(res.equals(table.getValue(ROW_TO_VALIDATE, COLUMN_TO_VALIDATE)));
 
+
+	}
+	@Test(dataProvider = "NegativeDataProvider")
+	public void verifyNegative(String firstNum, String secondNum, Operations value) {
+		String res = Calculator.calculate(firstNum,secondNum,value);
+		calculatorPage.setDataAndRun(firstNum,secondNum,value,res);
+		table.load();
+		Assert.assertTrue(res.equals(table.getValue(ROW_TO_VALIDATE, COLUMN_TO_VALIDATE)));
+
+
+	}
+
+
+	@DataProvider
+	public Object[][] ValidDataProvider() {
+		return new Object[][]{
+				{ "1","2", ADDITION },
+				{ "-2","4", ADDITION },
+				{ "-4","-2", ADDITION },
+				{ "1","2", DIVISION },
+				{ "-2","4", DIVISION },
+				{ "-4","-2", DIVISION },
+				{ "1","2", MULTIPLICATION },
+				{ "-2","4", MULTIPLICATION },
+				{ "-4","-2", MULTIPLICATION },
+				{ "1","2", SUBTRACTION },
+				{ "-2","4", SUBTRACTION },
+				{ "-4","-2", SUBTRACTION },
+				{ "1","2", MODULO },
+				{ "-2","4", MODULO },
+				{ "-4","-2", MODULO }
+
+		};
+	}
+
+	@DataProvider
+	public Object[][] NegativeDataProvider() {
+		return new Object[][]{
+				{ "ww","ww", ADDITION },
+				{ "","4", ADDITION },
+				{ "dd","-2", ADDITION },
+				{ "1","0", DIVISION },
+				{ "5","-0", DIVISION },
+				{ "-4","0", DIVISION },
+				{ "dd","dd", MULTIPLICATION },
+				{ "","", MULTIPLICATION },
+				{ "hh","-2", MULTIPLICATION },
+				{ "","2", SUBTRACTION },
+				{ "yy","yy", SUBTRACTION },
+				{ "","-hh", SUBTRACTION },
+				{ "","2", MODULO },
+				{ "h","4", MODULO },
+				{ "hh","-", MODULO }
+
+		};
 	}
 
 	@AfterTest
